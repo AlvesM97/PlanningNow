d9e34b8ac5f95fbeca9970ab45f1f09ee585eb23
AlvesM97 <gutomorais2002@gmail.com>
2023-03-16 12:24:37 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,19 +4,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application{
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
+public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader  fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
-        Parent root = fxmlLoader.load();
+        String path = "screens/home_screen.fxml";
+        Parent root = FXMLLoader.load(getClass().getResource(path));
         Scene tela = new Scene(root);
 
         primaryStage.setTitle("5Style");
         primaryStage.setScene(tela);
         primaryStage.show();
+
     }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
 }
