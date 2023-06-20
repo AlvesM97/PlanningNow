554b8014af687d92d38f372c0c326d9b3f9561bc
AlvesM97 <gutomorais2002@gmail.com>
2023-06-20 13:49:35 -0300

    Update CreateSchema.sql

--- SQL/CreateSchema.sql
+++ SQL/CreateSchema.sql
@@ -5,99 +5,96 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema 5style
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `5style` ;
-
--- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `5style` DEFAULT CHARACTER SET utf8 ;
-USE `5style` ;
+-- -----------------------------------------------------
+-- Schema planningnow
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Table `mydb`.`Roupa`
+-- Schema planningnow
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `5style`.`Roupa` (
-  `idRoupa` INT NOT NULL AUTO_INCREMENT,
-  `tamanho` VARCHAR(45) NOT NULL,
-  `cor` VARCHAR(45) NOT NULL,
+CREATE SCHEMA IF NOT EXISTS `planningnow` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `planningnow` ;
+
+-- -----------------------------------------------------
+-- Table `planningnow`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `planningnow`.`usuario` (
+  `ra` INT NOT NULL,
+  `senha` VARCHAR(30) NOT NULL,
+  `tipo` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
-  `valor` DECIMAL(45) NOT NULL,
-  `imagem` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idRoupa`, `tamanho`, `cor`))
-ENGINE = InnoDB;
+  `curso` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ra`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Usuario`
+-- Table `planningnow`.`monitoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `5style`.`Usuario` (
-  `idUsuario` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `senha` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idUsuario`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Compra`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `5style`.`Compra` (
-  `idCompra` INT NOT NULL AUTO_INCREMENT,
-  `valorTotal` DECIMAL(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `planningnow`.`monitoria` (
+  `idMonitoria` INT NOT NULL AUTO_INCREMENT,
+  `Usuario_ra` INT NOT NULL,
+  `data` VARCHAR(20) NOT NULL,
+  `hora` VARCHAR(20) NOT NULL,
+  `disciplina` VARCHAR(45) NOT NULL,
+  `sala` VARCHAR(20) NOT NULL,
   `quantidade` INT NOT NULL,
-  `Usuario_idUsuario` INT NOT NULL,
-  PRIMARY KEY (`idCompra`),
-  INDEX `fk_Compra_Usuario1_idx` (`Usuario_idUsuario` ASC) VISIBLE,
-  CONSTRAINT `fk_Compra_Usuario1`
-    FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `mydb`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  PRIMARY KEY (`idMonitoria`),
+  INDEX `fk_Monitoria_Usuario1_idx` (`Usuario_ra` ASC) INVISIBLE,
+  CONSTRAINT `fk_Monitoria_Usuario1`
+    FOREIGN KEY (`Usuario_ra`)
+    REFERENCES `planningnow`.`usuario` (`ra`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 26
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Quantidade`
+-- Table `planningnow`.`agenda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `5style`.`Quantidade` (
-  `quantidade` INT NOT NULL,
-  `Roupa_idRoupa` INT NOT NULL,
-  `Roupa_tamanho` VARCHAR(45) NOT NULL,
-  `Roupa_cor` VARCHAR(45) NOT NULL,
-  INDEX `fk_Quantidade_Roupa_idx` (`Roupa_idRoupa` ASC, `Roupa_tamanho` ASC, `Roupa_cor` ASC) VISIBLE,
-  CONSTRAINT `fk_Quantidade_Roupa`
-    FOREIGN KEY (`Roupa_idRoupa` , `Roupa_tamanho` , `Roupa_cor`)
-    REFERENCES `mydb`.`Roupa` (`idRoupa` , `tamanho` , `cor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `planningnow`.`agenda` (
+  `idAgenda` INT NOT NULL AUTO_INCREMENT,
+  `Usuario_ra` INT NOT NULL,
+  `monitoria_idMonitoria` INT NOT NULL,
+  PRIMARY KEY (`idAgenda`),
+  INDEX `fk_Agenda_Usuario1_idx` (`Usuario_ra` ASC) VISIBLE,
+  INDEX `fk_agenda_monitoria1_idx` (`monitoria_idMonitoria` ASC) VISIBLE,
+  CONSTRAINT `fk_agenda_monitoria1`
+    FOREIGN KEY (`monitoria_idMonitoria`)
+    REFERENCES `planningnow`.`monitoria` (`idMonitoria`),
+  CONSTRAINT `fk_Agenda_Usuario1`
+    FOREIGN KEY (`Usuario_ra`)
+    REFERENCES `planningnow`.`usuario` (`ra`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 63
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`RoupaCompra`
+-- Table `planningnow`.`publicacao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `5style`.`RoupaCompra` (
-  `Roupa_idRoupa` INT NOT NULL,
-  `Roupa_tamanho` VARCHAR(45) NOT NULL,
-  `Roupa_cor` VARCHAR(45) NOT NULL,
-  `Compra_idCompra` INT NOT NULL,
-  PRIMARY KEY (`Roupa_idRoupa`, `Roupa_tamanho`, `Roupa_cor`, `Compra_idCompra`),
-  INDEX `fk_Roupa_has_Compra_Compra1_idx` (`Compra_idCompra` ASC) VISIBLE,
-  INDEX `fk_Roupa_has_Compra_Roupa1_idx` (`Roupa_idRoupa` ASC, `Roupa_tamanho` ASC, `Roupa_cor` ASC) VISIBLE,
-  CONSTRAINT `fk_Roupa_has_Compra_Roupa1`
-    FOREIGN KEY (`Roupa_idRoupa` , `Roupa_tamanho` , `Roupa_cor`)
-    REFERENCES `mydb`.`Roupa` (`idRoupa` , `tamanho` , `cor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Roupa_has_Compra_Compra1`
-    FOREIGN KEY (`Compra_idCompra`)
-    REFERENCES `mydb`.`Compra` (`idCompra`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `planningnow`.`publicacao` (
+  `idPublicacao` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
+  `data` VARCHAR(45) NOT NULL,
+  `Usuario_ra` INT NOT NULL,
+  PRIMARY KEY (`idPublicacao`),
+  UNIQUE INDEX `idPublicacao_UNIQUE` (`idPublicacao` ASC) VISIBLE,
+  INDEX `fk_Publicacao_Usuario1_idx` (`Usuario_ra` ASC) VISIBLE,
+  CONSTRAINT `fk_Publicacao_Usuario1`
+    FOREIGN KEY (`Usuario_ra`)
+    REFERENCES `planningnow`.`usuario` (`ra`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 228
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
